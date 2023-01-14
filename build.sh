48f7f275ad4efe3db04c65fb407d35407a9ad66f
nyamage <tsukam.j@gmail.com>
2023-01-14 17:46:58 +0900

    add build.sh to change build command per branch

---
+++ build.sh
@@ -0,0 +1,14 @@
+# !/bin/bash
+
+if [ "$CF_PAGES_BRANCH" == "production" ]; then
+  # Run the "production" script in `package.json` on the "production" branch
+  # "production" should be replaced with the name of your Production branch
+  npm run prod-build
+elif [ "$CF_PAGES_BRANCH" == "staging" ]; then
+  # Run the "staging" script in `package.json` on the "staging" branch
+  # "staging" should be replaced with the name of your specific branch
+  npm run staging-build
+else
+  # Else run the dev script
+  npm run dev-build
+fi
