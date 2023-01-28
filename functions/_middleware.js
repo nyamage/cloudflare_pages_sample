b1f1b191d8848ea709fcee088d11ca228719735b
nyamage <tsukam.j@gmail.com>
2023-01-28 13:01:17 +0900

    fix

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -2,7 +2,7 @@ function authentication(context) {
   if (context.request.headers.get("x-email") != "admin@example.com") {
     return new Response("Unauthorized", { status: 403 });
   }
-  console.log(context)
+  console.log(`func:authentication context:${context}`)
   return context.next();
 }
 
