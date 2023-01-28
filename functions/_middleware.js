c5b87b971bd38afe783439588f6cdb05c7c545f9
nyamage <tsukam.j@gmail.com>
2023-01-28 16:05:30 +0900

    fix

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,9 +1,12 @@
 function authentication(context) {
-  console.log("func:authentication context:%O", context);
-  console.log("func:authentication context.request:%O", context.request);
+  console.log(`func:authentication context:${context}`);
   console.log(
-    "func:authentication context.request.headers:%O",
-    context.request.headers
+    `func:authentication context.request:${JSON.stringify(context.request)}`
+  );
+  console.log(
+    `func:authentication context.request.headers:${JSON.stringify(
+      context.request.headers
+    )}`
   );
   if (context.request.headers.get("x-email") != "admin@example.com") {
     return new Response("Unauthorized", { status: 403 });
